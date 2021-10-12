9ebe858e3eba3a595e74ca6dc6e96179530ba395
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-10-13 00:58:10 +0400

    Send Mail Done

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 UPLOAD_FOLDER = 'static/uploads'
 # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
